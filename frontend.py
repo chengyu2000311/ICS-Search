dfaf88ce0b062f6a2047ff0a5aaa95b56835d844
Haoyu Cheng <chengyu2000311@gmail.com>
2021-02-28 21:37:02 +0800

    update Web UI

--- frontend.py
+++ frontend.py
@@ -7,10 +7,11 @@ app=Flask(__name__)
 def index():
     query = request.args.get('query')
     urls = ['Result Place holder'] * 5
-    searchEni = search("./indexFile/10924TokenDocId.txt", "./indexFile/tf_idfMerge.txt")
-    if query != None:
+    searchEni = search("./indexFile/10176TokenDocId.txt", "./indexFile/tf_idfMerge.txt")
+    if query != None and query!= "":
         urls = searchEni.start(query)
     query = "Enter here" if query == None else query
+    print(urls)
     return render_template("index.html", one=urls[0], two=urls[1], three=urls[2],
         four=urls[3],five=urls[4], query=query)
 
