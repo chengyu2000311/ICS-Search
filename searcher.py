b315af466e04de854646328f0123e9288a2a299c
dinnerhe <61226161+dinnerhe@users.noreply.github.com>
2021-02-28 20:39:43 +0800

    Removed unnecessary stop words during searching

--- searcher.py
+++ searcher.py
@@ -1,5 +1,5 @@
 import os, Stemmer, linecache, time, json, io, multiprocessing, math
-
+from nltk.corpus import stopwords
 
 class search():
     def __init__(self, indexFile: str, tf_idfFile: str):
@@ -10,6 +10,8 @@ class search():
             self.Index_of_Token = json.load(iotfile)
         with open("DtU.json", 'r') as duf:
             self.DocId_to_URL = json.load(duf)
+        self.total_doc = len(self.DocId_to_URL)
+        self.stop_words = set(stopwords.words('english'))
 
     def _getIndexForAllQueries(self, fileName: str, tokens: list) -> list:
         res = list()
@@ -55,7 +57,7 @@ class search():
             doc_freq = len(Docs)
             buf = [[x[0], x[1]] for x in Docs]
             for i in range(doc_freq):
-                buf[i][0] = round((1 + math.log10(buf[i][0]) *  math.log10( 51478 / doc_freq)) , 2)
+                buf[i][0] = round((1 + math.log10(buf[i][0]) *  math.log10( self.total_doc/ doc_freq)) , 2)
             answer = sorted(buf, key=lambda x: -(x[0]))
             # Following are for debug in case indexer is wrong
             # for x in Docs:
@@ -73,13 +75,24 @@ class search():
             doc_freq = len(Docs)
             buf = [[x[0], x[1]] for x in Docs]
             for i in range(doc_freq):
-                buf[i][0] = round((1 + math.log10(buf[i][0]) * math.log10(51478 / doc_freq)), 2)
+                buf[i][0] = round((1 + math.log10(buf[i][0]) * math.log10(self.total_doc / doc_freq)), 2)
             answer = sorted(buf, key=lambda x: -(x[0]))
             res = [self.DocId_to_URL[str(x[1])] for x in answer]
             return res
         except KeyError:
             print('error')
-
+    def check_stop_word_freq(self, queries: list):
+        c = 0
+        result = set()
+        for word in queries:
+            if word in self.stop_words:
+                c+=1
+            else:
+                result.add(word)
+        if c/len(queries) > 0.4: #check if needs removing stop words
+            return list(set(queries))
+        else:
+            return list(result)
 
     def start(self, query: str):
         query = query.lower()
@@ -99,19 +112,20 @@ class search():
             end = time.time()
             print(f"completed in {end-start} seconds.")
         else:
+            queries = self.check_stop_word_freq(queries)
             allIndexs = self._getIndexForAllQueries(self.indexFile, queries)
             mid = time.time()
             answer = self._merge2Index(allIndexs[0], allIndexs[1])
             for i in range(2, len(queries)):
                 answer = self._merge2Index(answer, allIndexs[i])
                 # print(self.getUrl(answer[:5]))
-                end = time.time()
-                print(answer)
-                print("{} spent On searching DocID".format(mid - start))
-                print(f"Spent {end - mid} on sorting.\ncompleted in {end - start} seconds.")
+            end = time.time()
+            #print(answer)
+            print("{} spent On searching DocID".format(mid - start))
+            print(f"Spent {end - mid} on sorting.\ncompleted in {end - start} seconds.")
             return self.rankBasedOnAnd(answer)[:6]
 
 
 if __name__ == "__main__":
     searchEni = search("./indexFile/10176TokenDocId.txt", "./indexFile/tf_idfMerge.txt")
-    print(searchEni.start("machine learning"))
+    print(searchEni.start("master of computer science"))
